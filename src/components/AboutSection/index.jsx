36ba763d50b982945fc3a5c52e7f046b8c0407e3
JokerMSD <igorpn2004@gmail.com>
2023-09-27 17:56:20 -0300

    Feat: Change AboutSection p content

--- src/components/AboutSection/index.jsx
+++ src/components/AboutSection/index.jsx
@@ -6,7 +6,7 @@ function About() {
       <div className='About__container'>
         <h2 className='About__title'>Sobre mim</h2>
       </div>
-      <p className='About__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit...</p>
+      <p className='About__description'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin ex magna, imperdiet sodales sem quis, sollicitudin lobortis purus. Etiam a ipsum finibus, dictum leo non, ultrices dui. Nunc id felis pharetra, vehicula enim in, suscipit nisi. Mauris eget sapien a velit facilisis ullamcorper feugiat nec orci. Duis ac iaculis turpis, at feugiat orci.</p>
     </section>
   );
 }
