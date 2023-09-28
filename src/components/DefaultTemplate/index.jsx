3bc9c9f77dfe83917d71ae8b0e0bd24b15b1c9cd
JokerMSD <igorpn2004@gmail.com>
2023-09-28 14:37:49 -0300

    FEAT: Add DefaultTemplate dir

---
+++ src/components/DefaultTemplate/index.jsx
@@ -0,0 +1,15 @@
+import Header from "../Header/index"
+import Footer from "../Footer/index"
+
+
+export const DefaultTemplate = ({children}) => {
+  return (
+    <>
+      <Header />
+      <main>
+        {children}
+      </main>
+      <Footer />
+    </>
+  );
+};
--- src/pages/LandingPage/index.jsx
+++ src/pages/LandingPage/index.jsx
@@ -1,7 +1,6 @@
-import About from "../../components/AboutSection";
+import { DefaultTemplate } from "../../components/DefaultTemplate";
+import About from "../../components/AboutSection/index";
 import Banner from "../../components/BannerSection";
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
 import Projects from "../../components/ProjectSection";
 import Tech from "../../components/TechSection";
 
@@ -9,14 +8,12 @@ import Tech from "../../components/TechSection";
 export const LandingPage = () => {
   return (
     <>
-      <Header />
-      <main>
+      <DefaultTemplate>
         <Banner />
         <About />
         <Tech />
         <Projects />
-      </main>
-      <Footer />
+      </DefaultTemplate>
     </>
   );
 };
